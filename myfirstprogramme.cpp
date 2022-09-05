df7e84f21da5558f2170f59bcdc86c2dedf573f7
paulito02 <71513408+paulito02@users.noreply.github.com>
2022-09-06 00:47:17 +0100

    Add files via upload
    my first work on c++

---
+++ myfirstprogramme.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+cout << "hello world! \n\n";
+cout << "i am learning C++ \n\n";
+
+
+int mynum = 15, \n\n;
+cout << mynum;
+int myAge = 22;
+cout << " i am " << myAge << " years old.";
+return 0;
+}
+
