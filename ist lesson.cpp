e4d0492c44d035461f5dd09a8c1d710b2e2f0ac0
paulito02 <71513408+paulito02@users.noreply.github.com>
2022-09-22 10:03:26 +0100

    Add files via upload

---
+++ ist lesson.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+    int file_size = 0,;
+    int counter = 4;
+    double sales =9.99;
+    file_size = 100;
+    cout << "file_size";
+    return 0;
+}
