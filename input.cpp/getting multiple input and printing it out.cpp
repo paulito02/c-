decf29207d80a8b088027385d552826a226c60cd
paulito02 <71513408+paulito02@users.noreply.github.com>
2022-10-10 22:26:04 +0100

    Add files via upload
    working on various input in c++ both getting single input and getting bmultiple input from the users

---
+++ input.cpp/getting multiple input and printing it out.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+string fullname;
+cout<< "what is your fullname";
+getline(cin, fullname);
+cout<< "your name is:", cout<< fullname;
+}
