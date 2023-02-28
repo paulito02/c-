9e2dc43a01bc916634c4fb2b51ec31a2340f927a
paulito02 <71513408+paulito02@users.noreply.github.com>
2023-02-28 12:11:01 +0100

    Add files via upload
    using cout and cin in C++ to get in input and output together with the << and >>

---
+++ keywords.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+
+int favourite_number;
+std::cout<<"enter your favourite number between 1 and 100";
+std::cin>>favourite_number;
+std::cout<<"amazing!! Thats is my favourite number too!" << std::endl;
+std::cout<< "No really!!,"<<favourite_number <<"is my favourite number!" << std::endl;
+return 0;
+}
